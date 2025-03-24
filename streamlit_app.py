4966ccad642ef137b4e7f59d4d12b03bea542b48
Maxime Bataille <maximebataille95@gmail.com>
2025-03-24 15:13:56 +0100

    fff

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,8 +86,7 @@ with col2:
 time_1 = minutes_1 * 60 + secondes_1
 time_2 = minutes_2 * 60 + secondes_2
 
-speed_1 = time_1/ distance_1
-speed_2 = time_2 / distance_2
+
 
 
 
@@ -95,34 +94,38 @@ st.divider()
 
 st.subheader("Vitesse critique")
 
-cs = distance_1 / (time_1 + (time_2 - (distance_2/distance_1)*time_1)/(distance_2/distance_1 - 1))
+if distance_1 == 0 or distance_2 == 0 or time_1 == 0 or time_2 == 0:
+     st.warnings("Les distances et le temps ne peuvent être nuls")
+else:
 
-# convert speed m/s en allure mm:ss par km
-def calculate_pace(total_seconds, distance_meters):
-        pace_seconds_per_km = total_seconds / (distance_meters / 1000)
-        minutes, seconds = divmod(pace_seconds_per_km, 60)
-        return f"{int(minutes)} min {int(seconds):02d} s / km"
+    cs = distance_1 / (time_1 + (time_2 - (distance_2/distance_1)*time_1)/(distance_2/distance_1 - 1))
 
-st.write(f"Vitesse critique: {calculate_pace(1, cs)}")
+    # convert speed m/s en allure mm:ss par km
+    def calculate_pace(total_seconds, distance_meters):
+            pace_seconds_per_km = total_seconds / (distance_meters / 1000)
+            minutes, seconds = divmod(pace_seconds_per_km, 60)
+            return f"{int(minutes)} min {int(seconds):02d} s / km"
+
+    st.write(f"Vitesse critique: {calculate_pace(1, cs)}")
 
 
 
-zone1_max = round(0.75 * cs)
-zone2_max = round(0.85 * cs)
-zone3_max = round(0.93 * cs)
-zone4_max = round(1* cs)
+    zone1_max = round(0.75 * cs)
+    zone2_max = round(0.85 * cs)
+    zone3_max = round(0.93 * cs)
+    zone4_max = round(1* cs)
 
-st.subheader("Zones 1")
-st.write(f"{0} - {zone1_max}")
+    st.subheader("Zones 1")
+    st.write(f"{0} - {zone1_max}")
 
-st.subheader("Zones 2")
-st.write(f"{zone1_max} - {zone2_max}")
+    st.subheader("Zones 2")
+    st.write(f"{zone1_max} - {zone2_max}")
 
-st.subheader("Zones 3")
-st.write(f"{zone2_max} - {zone3_max}")
+    st.subheader("Zones 3")
+    st.write(f"{zone2_max} - {zone3_max}")
 
-st.subheader("Zones 4")
-st.write(f"{zone3_max} - {zone4_max}")
+    st.subheader("Zones 4")
+    st.write(f"{zone3_max} - {zone4_max}")
 
-st.subheader("Zones 5")
-st.write(f"{zone4_max} - et plus")
+    st.subheader("Zones 5")
+    st.write(f"{zone4_max} - et plus")
